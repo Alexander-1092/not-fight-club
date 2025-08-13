363912395d5fac1f132e8986309128212733fc16
Alexander-1092 <Alex-Sadr1092@yandex.ru>
2025-08-13 20:27:43 +0300

    feat: add adaptiv

--- js/newGame.js
+++ js/newGame.js
@@ -1,7 +1,45 @@
 const savedUser = localStorage.getItem("user");
 const user = JSON.parse(savedUser);
-
-const fieldImgUser = document.querySelector(".field__img-user");
-fieldImgUser.src = user.avatar;
 const fieldNameUser = document.querySelector(".field__name-user");
 fieldNameUser.textContent = user.name;
+const fieldImgUser = document.querySelector(".field__img-user");
+const enemy = {
+  enemy1: {
+    name: "Калакула",
+    attack: 10,
+    health: 150,
+    crit: 1.5,
+    avatar: "./assets/img/pers/enemy/enemy1.jpg",
+  },
+  enemy2: {
+    name: "Джегол",
+    attack: 15,
+    health: 110,
+    crit: 1.1,
+    avatar: "./assets/img/pers/enemy/enemy2.jpg",
+  },
+  enemy3: {
+    name: "Косонтур",
+    attack: 10,
+    health: 130,
+    crit: 2,
+    avatar: "./assets/img/pers/enemy/enemy3.jpg",
+  },
+  enemy4: {
+    name: "Бреннер",
+    attack: 20,
+    health: 150,
+    crit: 1.2,
+    avatar: "./assets/img/pers/enemy/enemy4.jpg",
+  },
+  enemy5: {
+    name: "Урактус",
+    attack: 25,
+    health: 100,
+    crit: 1.5,
+    avatar: "./assets/img/pers/enemy/enemy5.jpg",
+  },
+};
+
+fieldImgUser.src = user.avatar;
+const fieldImgEnemy = document.querySelector(".field__img-enemy");
